acc3b41de357d83892bb63bedea54edbe81b411a
gill-anupreetkaur-216675597 <46390207+gill-anupreetkaur-216675597@users.noreply.github.com>
2020-10-22 10:48:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage;  //images/dice1.png -images/dice6.png
+var state = history.state || {};
 
-var image1 = document.querySelectorAll("img")[0];
+var reloadCount = state.reloadCount || 0;
 
-image1.setAttribute("src",randomImageSource);
+if (performance.navigation.type === 1) { // Reload
 
-var randomNumber2 = Math.floor(Math.random() * 6) +1;
+  state.reloadCount = ++reloadCount;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  history.replaceState(state, null, document.URL);
 
-document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+} else if (reloadCount) {
 
-if(randomNumber1 > randomNumber2){
+  delete state.reloadCount;
 
-  document.querySelector("h1").innerHTML = "✌🏻 Winner is Player 1!";
+  reloadCount = 0;
+
+  history.replaceState(state, null, document.URL);
+
 }
-else if(randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = "Winner is Player 2!✌🏻";
+
+if (reloadCount > 0) {
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+  var randomImageSource = "images/" + randomDiceImage;  //images/dice1.png -images/dice6.png
+
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src",randomImageSource);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+  if(randomNumber1 > randomNumber2){
+
+    document.querySelector("h1").innerHTML = "✌🏻 Winner is Player 1!";
+  }
+  else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Winner is Player 2!✌🏻";
+  }
+  else{
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
 }
-else{
-  document.querySelector("h1").innerHTML = "Draw!"
-}
+
